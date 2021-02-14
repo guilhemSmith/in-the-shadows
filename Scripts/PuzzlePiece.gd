e0d16c87efbb33f37c55cf913b121c2eabec20b0
guilhem smith <smith.guilhem@gmail.com>
2021-02-15 00:31:34 +0100

    Add both rotation axis.

--- Scripts/PuzzlePiece.gd
+++ Scripts/PuzzlePiece.gd
@@ -1,20 +1,32 @@
 extends Spatial
 
+export var model: Resource = null
+
 export var enable_horizontal: bool = true
-export var horizontal_rot: int = 90
+export var horizontal_rot: int = 0
 export var horizontal_rot_goal: int = 0
 export var horizontal_rot_margin: int = 5
-export var horizontal_rot_scale: float = 0.25
+export var horizontal_rot_scale: float = 0.3
+
+export var enable_vertical: bool = true
+export var vertical_rot: int = 0
+export var vertical_rot_goal: int = 0
+export var vertical_rot_margin: int = 5
+export var vertical_rot_scale: float = 0.3
 
 export var ease_duration: float = 1.0
 export var validation_duration: float = 1.0
 
+onready var FORWARD = transform.basis.z.normalized()
+onready var UP = transform.basis.y.normalized()
 var selected: bool = true
 var rotating: bool = false
 onready var tween_node = $Tween
 onready var timer_validation = $Timer
 
 func _ready():
+	if model != null:
+		$Model.set_mesh(model)
 	rotation_degrees = Vector3(0, horizontal_rot, 0)
 
 func _input(event):
@@ -23,7 +35,9 @@ func _input(event):
 			rotating = event.is_pressed()
 		if rotating and event is InputEventMouseMotion:
 			if enable_horizontal:
-				horizontal_rot += event.relative.x * horizontal_rot_scale
+				horizontal_rot = event.relative.x * horizontal_rot_scale
+			if enable_vertical:
+				vertical_rot = event.relative.y * vertical_rot_scale
 			update_rot()
 			var valid = validate_angle(horizontal_rot, horizontal_rot_goal, horizontal_rot_margin)
 			if timer_validation.is_stopped() and valid:
@@ -32,17 +46,8 @@ func _input(event):
 				timer_validation.stop()
 
 func update_rot():
-	tween_node.interpolate_property(
-		self,
-		"rotation_degrees",
-		rotation_degrees,
-		Vector3(0, horizontal_rot, 0),
-		ease_duration,
-		Tween.TRANS_EXPO,
-		Tween.EASE_OUT
-	)
-	if not tween_node.is_active():
-		tween_node.start()
+	rotate(UP, deg2rad(horizontal_rot))
+	rotate(FORWARD, deg2rad(vertical_rot))
 
 func validate_angle(value, goal, margin):
 	return abs(dist_to_goal(value, goal)) <= margin
@@ -57,5 +62,5 @@ func dist_to_goal(value, goal):
 func _on_Timer_timeout():
 	print("validated")
 	var dist = dist_to_goal(horizontal_rot, horizontal_rot_goal)
-	horizontal_rot -= dist
+	horizontal_rot = -dist
 	update_rot()
