24d1f2d29b2ac1dffa8f63814cfb1d73061315dc
guilhem smith <smith.guilhem@gmail.com>
2021-02-18 22:56:54 +0100

    Add translation of puzzle.

--- Scripts/PuzzlePiece.gd
+++ Scripts/PuzzlePiece.gd
@@ -14,15 +14,21 @@ export var vertical_rot_goal: int = 0
 export var vertical_rot_margin: int = 5
 export var vertical_rot_scale: float = 0.3
 
+export var enable_translations: bool = true
+export var trans_horizontal: int = 0
+export var trans_vertical: int = 0
+export var trans_scale: float = 0.025
+
 export var ease_duration: float = 1.0
 export var validation_duration: float = 1.0
 
-#onready var FORWARD = -Vector3(-4.5, 0, 1.5).normalized()
-
 onready var FORWARD = transform.basis.z.normalized()
 onready var UP = transform.basis.y.normalized()
+
 var selected: bool = true
 var rotating: bool = false
+var translating: bool = false
+
 onready var tween_node = $Tween
 onready var timer_validation = $Timer
 
@@ -34,18 +40,28 @@ func _ready():
 func _input(event):
 	if selected:
 		if event is InputEventMouseButton and event.get_button_index() == 1:
-			rotating = event.is_pressed()
-		if rotating and event is InputEventMouseMotion:
-			if enable_horizontal:
-				horizontal_rot = event.relative.x * horizontal_rot_scale
-			if enable_vertical:
-				vertical_rot = event.relative.y * vertical_rot_scale
-			update_rot()
-			var valid = validate_angle(horizontal_rot, horizontal_rot_goal, horizontal_rot_margin)
-			if timer_validation.is_stopped() and valid:
-				timer_validation.start(validation_duration)
-			elif not timer_validation.is_stopped() and not valid:
-				timer_validation.stop()
+			if Input.is_action_pressed("player_ctrl"):
+				rotating = false
+				translating = event.is_pressed() and enable_translations
+			else:
+				rotating = event.is_pressed()
+				translating = false
+		if event is InputEventMouseMotion:
+			if rotating:
+				if enable_horizontal:
+					horizontal_rot = event.relative.x * horizontal_rot_scale
+				if enable_vertical:
+					vertical_rot = event.relative.y * vertical_rot_scale
+				update_rot()
+				var valid = validate_angle(horizontal_rot, horizontal_rot_goal, horizontal_rot_margin)
+				if timer_validation.is_stopped() and valid:
+					timer_validation.start(validation_duration)
+				elif not timer_validation.is_stopped() and not valid:
+					timer_validation.stop()
+			if translating:
+				trans_horizontal = event.relative.x
+				trans_vertical = -event.relative.y
+				translate(Vector3(trans_horizontal, trans_vertical, 0).normalized() * trans_scale)
 
 func update_rot():
 	rotate(UP, deg2rad(horizontal_rot))
