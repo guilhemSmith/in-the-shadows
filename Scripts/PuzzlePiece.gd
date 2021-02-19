eee30ae1b15e8ca21fc880b902c8cea32f05c6fb
Guilhem SMITH <gsmith@student.42.fr>
2021-02-19 14:26:21 +0100

    Rotation validation done.

--- Scripts/PuzzlePiece.gd
+++ Scripts/PuzzlePiece.gd
@@ -1,14 +1,16 @@
 extends Spatial
 
+signal rotation_valid
+
 export var mesh: Resource = null
 
 var rot_basis: Basis = Basis(Quat.IDENTITY)
 var rot_goal: Basis = Basis(Quat.IDENTITY)
-export var rot_margin: float = 1.0
+export var rot_margin: float = 0.25
 
-export var enable_h: bool = true
+export var enable_horizontal: bool = true
 export var h_scale: float = 0.01
-export var h_start: float = 0.0
+export var h_start: float = 42.0
 export var h_goal: float = 0.0
 
 export var enable_vertical: bool = true
@@ -26,27 +28,34 @@ var selected: bool = true
 var rotating: bool = false
 var translating: bool = false
 
-onready var tween_node = $Tween
-onready var timer_validation = $Timer
+export var validation_delay: float = 1.0
+
+onready var tween_node: Tween = $Tween
+onready var timer_validation: Timer = $Timer
 onready var model = $Model
 
 func _ready():
 	if model != null:
 		model.set_mesh(mesh)
 	
-	rot_goal = rot_goal.rotated(Vector3.UP, h_goal)
-	rot_goal = rot_goal.rotated(Vector3.FORWARD, v_goal)
+	rot_goal = rot_goal.rotated(Vector3.UP, h_goal).orthonormalized()
+	rot_goal = rot_goal.rotated(Vector3.FORWARD, v_goal).orthonormalized()
 	
-	rot_basis = rot_basis.rotated(Vector3.UP, h_start)
-	rot_basis = rot_basis.rotated(Vector3.FORWARD, v_start)
+	rot_basis = rot_basis.rotated(Vector3.UP, h_start).orthonormalized()
+	rot_basis = rot_basis.rotated(Vector3.FORWARD, v_start).orthonormalized()
 	model.transform.basis = rot_basis
 
 func _process(delta):
-	if ((transform.origin - t_offset).length() > 0.001):
+	if enable_translations:
 		transform.origin = lerp(transform.origin, t_offset, delta * 10)
-	else:
-		transform.origin = t_offset
-	model.transform.basis = model.transform.basis.slerp(rot_basis, delta * 10)
+
+	if enable_horizontal or enable_vertical:
+		model.transform.basis = model.transform.basis.slerp(rot_basis, delta * 10)
+		if selected:
+			if timer_validation.is_stopped() and is_valid():
+				timer_validation.start(validation_delay)
+			elif not timer_validation.is_stopped() and not is_valid():
+				timer_validation.stop()
 
 func _input(event):
 	if selected:
@@ -59,28 +68,26 @@ func _input(event):
 				translating = false
 		if event is InputEventMouseMotion:
 			if rotating:
-				if enable_h:
-					rot_basis = rot_basis.rotated(Vector3.UP, event.relative.x * h_scale)
+				if enable_horizontal:
+					rot_basis = rot_basis.rotated(Vector3.UP, event.relative.x * h_scale).orthonormalized()
 				if enable_vertical:
-					rot_basis = rot_basis.rotated(Vector3.FORWARD, event.relative.y * v_scale)
+					rot_basis = rot_basis.rotated(Vector3.FORWARD, event.relative.y * v_scale).orthonormalized()
 			if translating:
 				var trans = Vector3(event.relative.x, -event.relative.y, 0).normalized() * t_scale
 				t_offset.x = clamp(t_offset.x + trans.x, -t_limit_x, t_limit_x)
 				t_offset.y = clamp(t_offset.y + trans.y, -t_limit_y, t_limit_y)
 
+func select(select: bool):
+	selected = select
 
-#func validate_angle(value, goal, margin):
-#	return abs(dist_to_goal(value, goal)) <= margin
+func solve():
+	rot_basis = rot_goal
 
-#func dist_to_goal(value, goal):
-#	var angle = abs(value % 360 - goal)
-#	if angle < 360 - angle:
-#		return angle * sign(value)
-#	else:
-#		return (angle - 360) * sign(value)
+func is_valid():
+	var x = rot_goal.x - rot_basis.x
+	var y = rot_goal.y - rot_basis.y
+	var z = rot_goal.z - rot_basis.z
+	return x.length() + y.length() + z.length() < rot_margin
 
 func _on_Timer_timeout():
-	print("validated")
-#	var dist = dist_to_goal(horizontal_rot, horizontal_rot_goal)
-#	horizontal_rot = -dist
-#	update_rot()
+	emit_signal("rotation_valid")
