6771d2db1cbb6ddc2ae335659e428988927367bb
Guilhem SMITH <gsmith@student.42.fr>
2021-03-03 17:16:27 +0100

    Work on lvl selection presentation.

--- Scripts/LvlCube.gd
+++ Scripts/LvlCube.gd
@@ -5,6 +5,11 @@ var selected = false setget select
 export var unlocked = true setget unlock
 export var lvl = 1
 
+const COLOR_LOCKED: Color = Color.black
+const COLOR_SELECT: Color = Color.red
+const COLOR_IDLE: Color = Color.blue
+
+
 func _ready():
 	self.select(selected)
 	if Engine.editor_hint:
@@ -13,26 +18,35 @@ func _ready():
 		$Link.queue_free()
 	elif not unlocked:
 		var material = SpatialMaterial.new()
-		material.albedo_color = Color.darkgray
+		material.albedo_color = COLOR_LOCKED
 		$Link.material_override = material
 		$MeshInstance.material_override = material
 
 func unlock(val: bool):
 	unlocked = val
-	var material = SpatialMaterial.new()
-	if not unlocked:
-		material.albedo_color = Color.darkgray
 	if has_node("Link"):
-		$Link.material_override = material
-	$MeshInstance.material_override = material
-	self.select(selected)
+		var link_mat = SpatialMaterial.new()
+		if not unlocked:
+			link_mat.albedo_color = COLOR_LOCKED
+		else:
+			link_mat.albedo_color = COLOR_IDLE
+		$Link.material_override = link_mat
+		
+	var cube_mat = SpatialMaterial.new()
+	if not unlocked:
+		cube_mat.albedo_color = COLOR_LOCKED
+	elif not selected:
+		cube_mat.albedo_color = COLOR_IDLE
+	else:
+		cube_mat.albedo_color = COLOR_SELECT
+	$MeshInstance.material_override = cube_mat
 
 func select(val: bool):
 	if unlocked:
 		selected = val
 		var material = SpatialMaterial.new()
 		if (selected):
-			material.albedo_color = Color.red
+			material.albedo_color = COLOR_SELECT
 		else:
-			material.albedo_color = Color.blue
+			material.albedo_color = COLOR_IDLE
 		$MeshInstance.material_override = material
