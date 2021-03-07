7e3c0ba8238103245d805df11a75fbf708a5800c
guilhem smith <smith.guilhem@gmail.com>
2021-03-07 10:57:03 +0100

    Add button to stop music.

--- Scripts/MusicManager.gd
+++ Scripts/MusicManager.gd
@@ -3,6 +3,7 @@ extends AudioStreamPlayer
 export(Array, Resource) var musics
 
 var index: int
+var active = true
 
 func _ready():
 	if musics.size() > 0:
@@ -12,7 +13,15 @@ func _ready():
 		play()
 
 func _on_MusicManager_finished():
-	if musics.size() > 0:
+	if active and musics.size() > 0:
 		index = (index + 1) % musics.size()
 		set_stream(musics[index])
 		play()
+
+
+func _on_VolumeButton_pressed():
+	active = not active
+	if not active:
+		$AnimationPlayer.play("Stop")
+	else:
+		$AnimationPlayer.play("Start")
